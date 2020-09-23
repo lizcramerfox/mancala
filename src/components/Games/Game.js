82f657273124879b6300e66d0aab0f74f8b654c8
lizcramerfox <ecfox2008@gmail.com>
2020-09-23 17:05:39 -0400

    Game now displays all pockets, grouped by type

--- src/components/Games/Game.js
+++ src/components/Games/Game.js
@@ -15,15 +15,16 @@ class Pocket extends Component {
     const { pocket, stones } = this.props
     const { player, isMancala } = pocket
 
-    const playerClass = `player-${player}`
-    const typeClass = isMancala ? `mancala` : `non-mancala`
+    const playerClass = `player-${player.toLowerCase()}`
+    const type = isMancala ? `mancala` : `non-mancala`
     const pocketID = `pocket-${pocket.toString()}`
 
-    const classes = [playerClass, typeClass].join(' ')
+    const classes = [playerClass, type].join(' ')
+
 
     return (
       <Fragment>
-        <div className={classes} id={pocketID}>{pocket.toString()} : {stones}</div>
+        <div className={classes} id={pocketID} player={player} type={type}>{pocket.toString()} : {stones}</div>
       </Fragment>
     )
   }
@@ -32,13 +33,38 @@ class Pocket extends Component {
 class Board extends Component {
 
   render () {
-    const boardJsx = Array.from(this.props.board.pockets.entries()).map(([pocket, stones]) => {
-      return <Pocket pocket={pocket} stones={stones} key={pocket.toString()}/>
-    })
+    const entriesArray = Array.from(this.props.board.pockets.entries())
+
+    const pocketsA = entriesArray
+      .filter(([pocket, stones]) => pocket.player === "A" && pocket.isMancala === false)
+      .map(([pocket, stones]) => {
+        return <Pocket pocket={pocket} stones={stones} key={pocket.toString()}/>
+      })
+
+    const pocketsB = entriesArray
+      .filter(([pocket, stones]) => pocket.player === "B" && pocket.isMancala === false)
+      .map(([pocket, stones]) => {
+        return <Pocket pocket={pocket} stones={stones} key={pocket.toString()}/>
+      })
+
+    const mancalaA = entriesArray
+      .filter(([pocket, stones]) => pocket.player === "A" && pocket.isMancala === true)
+      .map(([pocket, stones]) => {
+        return <Pocket pocket={pocket} stones={stones} key={pocket.toString()}/>
+      })
+
+    const mancalaB = entriesArray
+      .filter(([pocket, stones]) => pocket.player === "B" && pocket.isMancala === true)
+      .map(([pocket, stones]) => {
+        return <Pocket pocket={pocket} stones={stones} key={pocket.toString()}/>
+      })
 
     return (
       <Fragment>
-        {boardJsx}
+        <div>Pockets-A: {pocketsA}</div>
+        <div>Pockets-B: {pocketsB}</div>
+        <div>Mancala-A: {mancalaA}</div>
+        <div>Mancala-B: {mancalaB}</div>
       </Fragment>
     )
   }
