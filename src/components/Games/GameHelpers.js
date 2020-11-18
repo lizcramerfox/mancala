3edbb3bcdf8891d7bc9e2c52159c3a47443460f2
lizcramerfox <ecfox2008@gmail.com>
2020-11-18 12:20:14 -0500

    Finishes refactors in GameIndexPreview, fixes game status labeling

--- src/components/Games/GameHelpers.js
+++ src/components/Games/GameHelpers.js
@@ -1,14 +1,21 @@
 export function mancalaStones(game, player) {
-    return Array.from(game.board.pockets.entries())
-      .find(([pocket, stones]) => pocket.player === player && pocket.isMancala === true)[1]
-  }
+  return Array.from(game.board.pockets.entries())
+    .find(([pocket, stones]) => pocket.player === player && pocket.isMancala === true)[1]
+}
 
 export function oppositePlayer(player) {
-    return player === 'A' ? 'B' : 'A'
-  }
+  return player === 'A' ? 'B' : 'A'
+}
 
+export function isWinner(game, player) {
+  if (game.isOver) {
+    return false
+  }
+  return mancalaStones(game, player) >= mancalaStones(game, oppositePlayer(player))
+}
 
 module.export = {
   mancalaStones,
-  oppositePlayer
+  oppositePlayer,
+  isWinner
 }
--- src/components/Games/GameIndexPreview.js
+++ src/components/Games/GameIndexPreview.js
@@ -1,28 +1,9 @@
 import React, { Component } from 'react'
 import { withRouter, Link } from 'react-router-dom'
 import './games.module.scss'
-import { mancalaStones, oppositePlayer } from './GameHelpers'
+import { mancalaStones, isWinner } from './GameHelpers'
 
 class GameIndexPreview extends Component {
-  // REFACTOR:
-  // mancalaStones(player) {
-  //   return Array.from(this.props.game.board.pockets.entries())
-  //     .find(([pocket, stones]) => pocket.player === player && pocket.isMancala === true)[1]
-  // }
-
-  // REFACTOR:
-  // oppositePlayer(player) {
-  //   return player === 'A' ? 'B' : 'A'
-  // }
-
-  // REFACTOR:
-  isWinner(player) {
-    if (!this.props.game.isOver) {
-      return false
-    }
-    return mancalaStones(this.props.game, player) >= mancalaStones(this.props.game, oppositePlayer(player))
-  }
-
   
   render() {
     let gameStatusLabel, playerStatusLabel, gameStatusClass
@@ -31,28 +12,22 @@ class GameIndexPreview extends Component {
       gameStatusLabel = 'In Progress'
       playerStatusLabel = `Player ${this.props.game.currentPlayer}'s Turn`
       gameStatusClass = 'in-progress'
-    }
-
-    if (this.isWinner('A') && this.isWinner('B')) {
+    } else if (isWinner(this.props.game, 'A') && isWinner(this.props.game, 'B')) {
       gameStatusLabel = 'GAME OVER'
       playerStatusLabel = 'Tie Game'
       gameStatusClass = 'game-over'
-    }
-
-    if (this.isWinner('A') && !this.isWinner('B')) {
+    } else if (isWinner(this.props.game, 'A') && !isWinner(this.props.game, 'B')) {
       gameStatusLabel = 'GAME OVER'
       playerStatusLabel = 'Player A Wins'
       gameStatusClass = 'game-over'
-    }
-
-    if (this.isWinner('B') && !this.isWinner('A')) {
+    } else {
       gameStatusLabel = 'GAME OVER'
       playerStatusLabel = 'Player B Wins'
       gameStatusClass = 'game-over'
     }
 
     const classNames = ["game-preview", gameStatusClass].join(' ')
-  
+
     return (
       <Link to={`/games/${this.props.id}`} className={classNames}>
         <div className="game-status-label">{gameStatusLabel}</div>
