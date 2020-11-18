91268f0c8027473b43ddb9ab804479d203ae8e4b
lizcramerfox <ecfox2008@gmail.com>
2020-11-18 13:22:21 -0500

    Refactors GameInfo

--- src/components/Games/GameBoard/GameInfo.js
+++ src/components/Games/GameBoard/GameInfo.js
@@ -1,38 +1,21 @@
 import React, { Component, Fragment } from 'react'
 import './gameboard.module.scss'
+import { mancalaStones, isWinner } from '../GameHelpers'
 
 class GameInfo extends Component {
-  // REFACTOR:
-  mancalaStones(player) {
-    return Array.from(this.props.game.board.pockets.entries())
-      .find(([pocket, stones]) => pocket.player === player && pocket.isMancala === true)[1]
-  }
-
-  // REFACTOR:
-  oppositePlayer(player) {
-    return player === 'A' ? 'B' : 'A'
-  }
-
-  // REFACTOR:
-  isWinner(player) {
-    if (!this.props.game.isOver) {
-      return false
-    }
-    return this.mancalaStones(player) >= this.mancalaStones(this.oppositePlayer(player))
-  }
-
+  
   render() {   
     let winnerClass
 
-    if (this.isWinner('A') && !this.isWinner('B')) {
+    if (isWinner(this.props.game, 'A') && !isWinner(this.props.game, 'B')) {
       winnerClass = 'a'
     }
 
-    if (this.isWinner('B') && !this.isWinner('A')) {
+    if (isWinner(this.props.game, 'B') && !isWinner(this.props.game, 'A')) {
       winnerClass = 'b'
     }
 
-    if (this.isWinner('A') && this.isWinner('B')) {
+    if (isWinner(this.props.game, 'A') && isWinner(this.props.game, 'B')) {
       winnerClass = 'tie'
     }
 
@@ -45,15 +28,15 @@ class GameInfo extends Component {
       infoJsx = <Fragment>Player <span className={classNamesCurrentGame}>{this.props.game.currentPlayer}</span>'s Turn</Fragment>
     }
     
-    if (this.props.game.isOver && (this.mancalaStones('A') > this.mancalaStones('B'))) {
+    if (this.props.game.isOver && (mancalaStones(this.props.game, 'A') > mancalaStones(this.props.game, 'B'))) {
       infoJsx = <Fragment>Player <span className={classNamesGameOver}>A</span> Wins!</Fragment>
     }
     
-    if (this.props.game.isOver && (this.mancalaStones('A') < this.mancalaStones('B'))) {
+    if (this.props.game.isOver && (mancalaStones(this.props.game, 'A') < mancalaStones(this.props.game, 'B'))) {
       infoJsx = <Fragment>Player <span className={classNamesGameOver}>B</span> Wins!</Fragment>
     }
 
-    if (this.props.game.isOver && (this.mancalaStones('A') === this.mancalaStones('B'))) {
+    if (this.props.game.isOver && (mancalaStones(this.props.game, 'A') === mancalaStones(this.props.game, 'B'))) {
       infoJsx = <Fragment><span className={classNamesGameOver}>Tie Game!</span></Fragment>
     }
 
