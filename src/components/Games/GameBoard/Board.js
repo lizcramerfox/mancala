706d5382930492137b240502972eb64c44eddadc
lizcramerfox <ecfox2008@gmail.com>
2020-11-18 13:17:05 -0500

    Refactors Board

--- src/components/Games/GameBoard/Board.js
+++ src/components/Games/GameBoard/Board.js
@@ -1,28 +1,9 @@
 import React, { Component } from 'react'
 import './gameboard.module.scss'
 import Pocket from './Pocket'
+import { isWinner } from '../GameHelpers'
 
 class Board extends Component {
-  // REFACTOR:
-  mancalaStones(player) {
-    return Array.from(this.props.game.board.pockets.entries())
-      .find(([pocket, stones]) => pocket.player === player && pocket.isMancala === true)[1]
-  }
-  
-  // REFACTOR:
-  oppositePlayer(player) {
-    return player === 'A' ? 'B' : 'A'
-  }
-
-  // REFACTOR:
-  isWinner(player) {
-    if (!this.props.game.isOver) {
-      return false
-    }
-
-    return this.mancalaStones(player) >= this.mancalaStones(this.oppositePlayer(player))
-  }
-
   render() {
     const entriesArray = Array.from(this.props.game.board.pockets.entries())
 
@@ -42,13 +23,13 @@ class Board extends Component {
     const mancalaA = entriesArray
       .filter(([pocket, stones]) => pocket.player === "A" && pocket.isMancala === true)
       .map(([pocket, stones]) => {
-        return <Pocket isWinner={this.isWinner('A')} game={this.props.game} pocket={pocket} stones={stones} key={pocket.toString()} />
+        return <Pocket isWinner={isWinner(this.props.game, 'A')} game={this.props.game} pocket={pocket} stones={stones} key={pocket.toString()} />
       })
 
     const mancalaB = entriesArray
       .filter(([pocket, stones]) => pocket.player === "B" && pocket.isMancala === true)
       .map(([pocket, stones]) => {
-        return <Pocket isWinner={this.isWinner('B')} game={this.props.game} pocket={pocket} stones={stones} key={pocket.toString()} />
+        return <Pocket isWinner={isWinner(this.props.game, 'B')} game={this.props.game} pocket={pocket} stones={stones} key={pocket.toString()} />
       })
    
     return (
